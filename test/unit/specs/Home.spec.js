44dc7e23976dad15ba0e35a0592f927b77c4c4d2
misoguy <misoguy1985@gmail.com>
2017-11-18 14:56:54 +0900

    Fix test file

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
 
-describe('HelloWorld.vue', () => {
+describe('Home.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
+    const Constructor = Vue.extend(Home);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('h1').textContent)
-    .toEqual('Insert Token');
+      .toEqual('Insert Token');
   });
 });
