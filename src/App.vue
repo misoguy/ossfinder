5276a3c6433113698261af58d9ddaa461bfc2090
misoguy <misoguy1985@gmail.com>
2017-11-21 11:58:19 +0900

    Fix App navigation to use "to" props

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
       </router-link>
       <v-spacer />
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat @click="navigateTo('/')">Home</v-btn>
+        <v-btn flat to="/">Home</v-btn>
         <template v-if="me">
-          <v-btn flat @click="navigateTo('profile')">Profile</v-btn>
-          <v-btn flat @click="navigateTo('issues')">Issues</v-btn>
+          <v-btn flat to="/profile">Profile</v-btn>
+          <v-btn flat to="/issues">Issues</v-btn>
           <v-btn flat @click="logout">Logout</v-btn>
         </template>
-        <v-btn v-else flat @click="navigateTo('login')">Login</v-btn>
+        <v-btn v-else flat to="/login">Login</v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
@@ -27,19 +27,19 @@
       app
       class="hidden-md-and-up"
       :value="true"
-      :active="activeNav"
+      :active="currentPath"
       color="white"
     >
-      <v-btn flat color="teal" value="/" @click="navigateTo('/')">
+      <v-btn flat color="teal" value="/" exact to="/">
         <span>Home</span>
         <v-icon>home</v-icon>
       </v-btn>
       <template v-if="me">
-        <v-btn flat color="teal" value="/profile" @click="navigateTo('profile')">
+        <v-btn flat color="teal" value="/profile" to="/profile">
           <span>Profile</span>
           <v-icon>account_circle</v-icon>
         </v-btn>
-        <v-btn flat color="teal" value="/issues" @click="navigateTo('issues')">
+        <v-btn flat color="teal" value="/issues" to="/issues">
           <span>Issues</span>
           <v-icon>list</v-icon>
         </v-btn>
@@ -48,7 +48,7 @@
           <v-icon>launch</v-icon>
         </v-btn>
       </template>
-      <v-btn v-else flat color="teal" value="/login" @click="navigateTo('login')">
+      <v-btn v-else flat color="teal" value="/login" to="/login">
         <span>Login</span>
         <v-icon>input</v-icon>
       </v-btn>
@@ -64,15 +64,8 @@
     created() {
       const token = localStorage.getItem('token');
       if (token) {
-        this.$store.dispatch('login', token).then(() => {
-          this.activeNav = this.currentPath;
-        });
-      } else {
-        this.activeNav = this.currentPath;
+        this.$store.dispatch('login', token);
       }
-    },
-    data() {
-      return { activeNav: '' };
     },
     computed: {
       ...mapGetters([
@@ -81,24 +74,12 @@
       ]),
     },
     methods: {
-      closeDrawer() {
-        this.drawer = false;
-      },
-      navigateTo(dest) {
-        this.$router.push(dest);
-        if (dest === '/') {
-          this.activeNav = '/';
-        } else {
-          this.activeNav = `/${dest}`;
-        }
-      },
       logout() {
         /* eslint-disable no-alert */
         /* eslint-disable no-restricted-globals */
         if (confirm('All data will be lost. Confirm Logout?')) {
           this.$store.dispatch('logout').then(() => {
             this.$router.push('/');
-            this.activeNav = '/';
           });
         }
       },
