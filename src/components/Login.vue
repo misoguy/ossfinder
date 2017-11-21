a4e458c38d2bcbb9975112621ca1597c5f4b0fa8
yuyuny <yuyunykk@gmail.com>
2017-11-21 19:51:03 +0900

    Add github token link

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,34 @@
 <template>
   <v-container id="login">
-    <v-text-field
-      label="Personal access tokens"
-      placeholder="Enter Personal access tokens"
-      v-model="token"
-    >
-    </v-text-field>
-    <v-btn @click="login(token)" color="primary">Login</v-btn>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-title primary-title>
+            Sign in
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              label="Personal access tokens"
+              placeholder="Enter Personal access tokens"
+              v-model="token"
+            >
+            </v-text-field>
+            <v-btn @click="login(token)" block color="primary">
+              Sign in
+            </v-btn>
+            <h6 class="body-1">or</h6>
+            <v-btn
+              block
+              color="secondary"
+              href="https://github.com/settings/tokens/new"
+            >
+              <img src="static/Github.png">
+              Get token from Github
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -27,3 +49,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  h6 {
+    text-align: center;
+  }
+
+  img {
+    width: 1.4rem;
+    margin-right: 0.5rem;
+  }
+</style>
