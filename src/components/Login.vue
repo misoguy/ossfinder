177420d2a5d8a5db936dab859adcf12998ed3c7a
misoguy <misoguy1985@gmail.com>
2017-11-23 15:08:17 +0900

    Add required scopes into login view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,46 @@
 <template>
-  <v-container id="login">
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-card-title primary-title>
-            Sign in
-          </v-card-title>
-          <v-card-text>
-            <v-text-field
-              label="Personal access tokens"
-              placeholder="Enter Personal access tokens"
-              v-model="token"
-            >
-            </v-text-field>
-            <v-btn @click="login" block color="primary">
-              Sign in
-            </v-btn>
-            <h6 class="body-1">or</h6>
-            <v-btn
-              block
-              color="secondary"
-              href="https://github.com/settings/tokens/new"
-              target="_blank"
-            >
-              <img src="../assets/GitHub.png" />
-              Get token from Github
-            </v-btn>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+  <v-container grid-list-md id="login">
+    <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg4 offset-lg4>
+      <v-card>
+        <v-card-title primary-title>
+          LOGIN
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            label="Personal access tokens"
+            placeholder="Enter Personal access tokens"
+            v-model="token"
+          >
+          </v-text-field>
+          <v-btn @click="login" block color="primary">
+            LOGIN
+          </v-btn>
+          <v-divider />
+          <v-btn
+            block
+            color="secondary"
+            href="https://github.com/settings/tokens/new"
+            target="_blank"
+          >
+            <img src="../assets/GitHub.png" />
+            Get token from Github
+          </v-btn>
+          <v-layout row wrap justify-center align-center>
+            <h4 class="body-2">REQUIRED SCOPES:</h4>
+            <div>
+              <v-chip disabled outline label color="blue-grey">
+                <v-icon>public</v-icon>
+                <h4>public_repo</h4>
+              </v-chip>
+              <v-chip disabled outline label color="blue-grey">
+                <v-icon>face</v-icon>
+                <h4>read:user</h4>
+              </v-chip>
+            </div>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-flex>
   </v-container>
 </template>
 
